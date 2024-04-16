66fe462accef6610c87968ad4da2470933beec66
Tim Hårek Andreassen <tim@harek.no>
2024-04-16 09:07:13 +0200

    docs: Add jsdocs
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- src/util.ts
+++ src/util.ts
@@ -19,6 +19,11 @@ async function _fetch(url: string | URL): Promise<unknown> {
   return result;
 }
 
+/**
+ * Search for book based on `query` from Open Library's API.
+ *
+ * @returns Search results
+ */
 export async function searchBook(query: string): Promise<Search> {
   const url = API_URL;
   url.pathname = '/search.json';
@@ -28,6 +33,11 @@ export async function searchBook(query: string): Promise<Search> {
   return Search.parse(result);
 }
 
+/**
+ * Get book based on its Open Library's ID, `id`, from Open Library's API.
+ *
+ * @returns Book
+ */
 export async function getBook(id: string): Promise<Book> {
   const url = API_URL;
   url.pathname = `/works/${id}.json`;
@@ -36,6 +46,11 @@ export async function getBook(id: string): Promise<Book> {
   return Book.parse(result);
 }
 
+/**
+ * Get author based on its Open Library's ID, `id`, from Open Library's API.
+ *
+ * @returns Author
+ */
 export async function getAuthor(id: string): Promise<Author> {
   const url = API_URL;
   url.pathname = `/authors/${id}.json`;
@@ -44,6 +59,11 @@ export async function getAuthor(id: string): Promise<Author> {
   return Author.parse(result);
 }
 
+/**
+ * Get book based on one of its ISBNs, `isbn`, from Open Library's API.
+ *
+ * @returns Book
+ */
 export async function getBookByISBN(isbn: string): Promise<Book> {
   const url = API_URL;
   url.pathname = `isbn/${isbn}.json`;
@@ -59,6 +79,11 @@ export async function getBookByISBN(isbn: string): Promise<Book> {
   return Book.parse(bookResult);
 }
 
+/**
+ * Get a book subject based on its key (snake-cased of actual title), `key`, from Open Library's API.
+ *
+ * @returns Subject
+ */
 export async function getSubject(key: string): Promise<SubjectResult> {
   const url = API_URL;
   url.pathname = `/subjects/${key}.json`;
